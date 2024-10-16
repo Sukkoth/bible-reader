b4f6e368edc46838ab7e8a04093869394a1f822f
Sukkoth <suukootj@gmail.com>
2024-10-16 08:42:04 +0300

    modify landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
 import GetStartedButton from "@/components/home/GetStartedButton";
 import { GET_USER } from "@/utils/supabase/services";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const authData = await GET_USER();
+  const authData = await GET_USER({
+    withRedirect: false,
+  });
+
   if (authData.user) {
     redirect("/home");
   }
   return (
     <main className='h-full w-full center-all flex-col'>
-      <Image
-        src='/background.jpg'
-        alt='bg'
-        fill
-        style={{ objectFit: "cover" }}
-        className='-z-10'
-      />
+      <div className='absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]'></div>
 
-      <h1 className='text-4xl font-bold text-white'>Bible Reader</h1>
-      <h2 className='pt-2 text-center px-7 text-white'>
+      <h1 className='text-6xl font-bold text-white'>Bible Reader</h1>
+      <h2 className={`pt-2 text-center px-7 text-white text-sm`}>
         Track your daily Bible reading journey with ease.
       </h2>
       <GetStartedButton />
