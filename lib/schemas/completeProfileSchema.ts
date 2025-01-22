770fbe2e469ec7b3654d9dab4751dad9f8c68e2d
Sukkoth <suukootj@gmail.com>
2025-01-22 16:44:35 +0300

    fix complete-profile bug (limited email characters)

--- lib/schemas/completeProfileSchema.ts
+++ lib/schemas/completeProfileSchema.ts
@@ -1,19 +1,19 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const CompleteProfileSchema = z.object({
   firstName: z
     .string({
-      required_error: "First name is required",
+      required_error: 'First name is required',
     })
-    .min(3, "First name should be atleast 3 characters")
+    .min(3, 'First name should be atleast 3 characters')
     .max(20),
   lastName: z.string().max(20).optional(),
   telegram_id: z.string().max(20).optional(),
-  email: z.string().email().max(20).optional(),
+  email: z.string().email().optional(),
   gender: z.string({
-    required_error: "Gender field is required",
+    required_error: 'Gender field is required',
   }),
-  avatar: z.string().startsWith("https://").optional(),
+  avatar: z.string().startsWith('https://').optional(),
 });
 
 export type CompleteProfileSchemaType = z.infer<typeof CompleteProfileSchema>;
